d7a3a7489e90ae78ad776453fe055e4d97dac5e3
hus-sin <hussin.sa@gmail.com>
2025-08-27 23:35:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,6 @@ if "page" not in st.session_state:
                                                                                                                                                             st.session_state.current_user = username
                                                                                                                                                             st.session_state.user_role = user_row.iloc[0]["Role"]
                                                                                                                                                             st.session_state.page = "home"
-                                                                                                                                                            else:
                                                                                                                                                                                                                                             st.error("اسم المستخدم أو كلمة المرور غير صحيحة.")
 
                                                                                                                                                                                                                                             # --- الصفحة الرئيسية ---
